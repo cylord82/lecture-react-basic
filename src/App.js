4366d4c7c741935b3b8e22e546874c33a8ed4024
Leo Sohn <lsohn@riotgames.com>
2018-07-01 12:02:25 +0900

    강의 목표를 출력하는 체크 리스트 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const goals = [
+  'React 개발에 필요한 환경을 구축한다.',
+  '새로운 자바스크립트 문법을 익힌다. ES2015(ES6), ES7, ES8',
+  '개발 편의를 위한 VSCode IDE를 익힌다.',
+  '기본적인 Git 사용법을 익힌다.',
+  'PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.',
+  'React 로 간단한 노트 앱을 만들어본다.',
+];
+
+const LectureGoalList = () => (
+  <div className="LectureGoalList">
+    <div>
+      강의 목표
+    </div>
+    <ul>
+      {goals.map((goal, idx) => (
+        <li key={idx}>
+          <label><input type="checkbox" /> {goal}</label>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +37,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <LectureGoalList />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,23 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-
-const myElem = (
-  <p>
-    이렇게 써도 되는거냐?
-  </p>
-)
-
-const myCom = () => (
-  <p>
-    이렇게 써도 되는거냐?
-  </p>
-)
-
-
-console.log(myElem, myCom(), <myCom />);
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
